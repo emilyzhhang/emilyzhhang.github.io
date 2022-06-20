6e60584d45b653d5c4350d8a46fb2eb11b653b85
emilyzhhang <107828475+emilyzhhang@users.noreply.github.com>
2022-06-19 21:38:08 -0700

    uploaded factfinder for the first time

---
+++ Emily's Fact Finder/script.js
@@ -0,0 +1,64 @@
+document.body.style.backgroundColor = "#B3B7D1";
+
+factform.a.addEventListener("click", showFact1);
+
+function showFact1(){
+	document.querySelector("#factdisplay").innerHTML = "Spider webs were used as bandages in ancient times. <img src='images/spiderWebs.jpg' alt='spider web'>"; 
+	document.querySelector('body').style.backgroundColor = "#7D4600";
+	document.querySelector('main').style.backgroundColor = "#E4D6A7";
+	document.querySelector('#factdisplay').style.color = "#3D51A4";
+	document.querySelector('#factform').style.backgroundColor = "#7E8287";
+	document.querySelector('#factform').style.borderColor = "#2E2836";
+	
+
+}//showFact1
+
+factform.b.addEventListener("click", showFact2);
+
+function showFact2(){
+	document.querySelector("#factdisplay").innerHTML = "Koala fingerprints are almost indistinguishable from humans’ — so much so, they can taint crime scenes! <img src='images/koala.jpg' alt='koala'>";
+	document.querySelector('body').style.backgroundColor = "#5D7583";
+	document.querySelector('main').style.backgroundColor = "#B6D3E2";
+	document.querySelector('#factdisplay').style.color = "#535465";
+	document.querySelector('#factform').style.backgroundColor = "#C89FA3"; 
+	document.querySelector('#factform').style.borderColor = "#84AEA2";
+	
+}//showFact2
+document.getElementById("b");
+
+factform.c.addEventListener("click", showFact3);
+
+function showFact3(){
+	document.querySelector("#factdisplay").innerHTML = "People are more creative in the shower. When we take a warm shower, we experience an increased dopamine flow that makes us more creative.<img src='images/shower.jpg' alt='shower head'>"
+	document.querySelector('body').style.backgroundColor = "#9AE4FE";
+	document.querySelector('main').style.backgroundColor = "#81B0EF";
+	document.querySelector('#factdisplay').style.color = "#4937D2";
+	document.querySelector('#factform').style.backgroundColor = "#0E958F"; 
+	document.querySelector('#factform').style.borderColor = "#7EB486";
+	
+	
+}//showFact3
+
+factform.d.addEventListener("click", showFact4);
+
+function showFact4(){
+	document.querySelector("#factdisplay").innerHTML = "You can hear a blue whale's heartbeat from over 2 miles away. Blue whales weigh an average of between 130,000 and 150,000kg, with their hearts weighing roughly 180kg.<img src='images/whale.jpg' alt='blue whale'>"
+	document.querySelector('body').style.backgroundColor = "#1D3C5E";
+	document.querySelector('main').style.backgroundColor = "#778DA9";
+	document.querySelector('#factdisplay').style.color = "#415A77";
+	document.querySelector('#factform').style.backgroundColor = "#5777B2"; 
+	document.querySelector('#factform').style.borderColor = "#C3C6BE";
+	
+}//showFact4
+
+factform.e.addEventListener("click", showFact5);
+
+function showFact5(){
+	document.querySelector("#factdisplay").innerHTML = "During an hour's swimming at a municipal pool, you will ingest 1/12 liter of urine.<img src='images/pool.jpg' alt='swimming pool'>"
+	document.querySelector('body').style.backgroundColor = "#1FC7FF";
+	document.querySelector('main').style.backgroundColor = "#2541B2";
+	document.querySelector('#factdisplay').style.color = "#00B89F";
+	document.querySelector('#factform').style.backgroundColor = "#2468F9"; 
+	document.querySelector('#factform').style.borderColor = "#FFF4AE";
+	
+}//showFact5
